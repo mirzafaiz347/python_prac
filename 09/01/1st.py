202de78393a2e63d49b9d9ba2e754df7da7f01c4
Farhan B <SBMPC.Student@SVKMGRP.COM>
2026-01-09 09:59:42 +0530

    this is commited

--- 09/01/1st.py
+++ 09/01/1st.py
@@ -1,24 +1,43 @@
 def oddeven():
-    
-    num = int(input("Enter a number"))
-    if(num % 2 == 0):
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
         print(f"{num} is even")
     else:
         print(f"{num} is odd")    
-        
-def oddOrEvenRange():
-    lower =int(input("Enter a lower number"))
-    upper =int(input("Enter a upper number"))         
 
-    for num in range(lower, upper+1):
-        if(num % 2 == 0):
+def oddOrEvenRange():
+    lower = int(input("Enter lower number: "))
+    upper = int(input("Enter upper number: "))         
+
+    for num in range(lower, upper + 1):
+        if num % 2 == 0:
             print(f"{num} is even")
         else:
             print(f"{num} is odd") 
-def main():
-    # oddeven() 
-    oddOrEvenRange()  
-    
-if __name__ == "__main__":
-    main()        
 
+def reversal():
+    num = int(input("Enter a number: "))
+    original_num = num
+    reversed_num = 0
+
+    while num > 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num = num // 10
+
+    print(f"{original_num} reversed is {reversed_num}")
+    
+def factorial(num):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(num-1)
+    
+def main():
+    # oddeven()
+    # oddOrEvenRange()
+    reversal()
+    factorial()
+
+if __name__ == "__main__":
+    main()
